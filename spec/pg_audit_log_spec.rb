a9f1c2a3694f46d1205a7254cbac2db726721dc0
Michael Schubert <michael@schubert.cx>
2011-09-09 00:19:02 -0400

    Stub as "User"

--- spec/pg_audit_log_spec.rb
+++ spec/pg_audit_log_spec.rb
@@ -55,7 +55,7 @@ describe PgAuditLog do
 
         context "when a user is present" do
           before do
-            Thread.current[:current_user] = stub(:id => 1, :unique_name => "my current user")
+            Thread.current[:current_user] = stub("User", :id => 1, :unique_name => "my current user")
             AuditedModel.create!
           end
 
