13d78ee70e0ba75d298513ba03b20dd58f1e135d
Michael Schubert <pair+schubert@pivotallabs.com>
2012-02-09 12:25:59 -0500

    Further optimize the audit log function by only calling the temporary functions once and not in the loop. Additionally do not set the primary key value once it has already been set.

--- lib/pg_audit_log/function.rb
+++ lib/pg_audit_log/function.rb
@@ -70,31 +70,30 @@ module PgAuditLog
                        AND pg_attribute.attnum = any(pg_index.indkey)
                        AND indisprimary'
               INTO primary_key_column USING TG_RELNAME;
+              user_identifier := pg_temp.pg_audit_log_user_identifier();
+              unique_name := pg_temp.pg_audit_log_user_unique_name();
+              primary_key_value := NULL;
 
               FOR col IN SELECT * FROM information_schema.columns WHERE table_name = TG_RELNAME LOOP
                 new_value := NULL;
                 old_value := NULL;
-                primary_key_value := NULL;
-                user_identifier := pg_temp.pg_audit_log_user_identifier();
-                unique_name := pg_temp.pg_audit_log_user_unique_name();
                 column_name := col.column_name;
-
-                IF TG_OP = 'INSERT' OR TG_OP = 'UPDATE' THEN
-                  EXECUTE 'SELECT CAST($1 . '|| column_name ||' AS TEXT)' INTO new_value USING NEW;
-                  IF primary_key_column IS NOT NULL THEN
-                    EXECUTE 'SELECT CAST($1 . '|| primary_key_column ||' AS VARCHAR)' INTO primary_key_value USING NEW;
-                  END IF;
-                END IF;
-                IF TG_OP = 'DELETE' OR TG_OP = 'UPDATE' THEN
-                  EXECUTE 'SELECT CAST($1 . '|| column_name ||' AS TEXT)' INTO old_value USING OLD;
-                  IF primary_key_column IS NOT NULL THEN
-                    EXECUTE 'SELECT CAST($1 . '|| primary_key_column ||' AS VARCHAR)' INTO primary_key_value USING OLD;
-                  END IF;
-                END IF;
-
                 IF TG_RELNAME = '#{users_table_name}' AND column_name = '#{users_access_column}' THEN
                   NULL;
                 ELSE
+                  IF TG_OP = 'INSERT' OR TG_OP = 'UPDATE' THEN
+                    EXECUTE 'SELECT CAST($1 . '|| column_name ||' AS TEXT)' INTO new_value USING NEW;
+                    IF primary_key_value IS NULL AND primary_key_column IS NOT NULL THEN
+                      EXECUTE 'SELECT CAST($1 . '|| primary_key_column ||' AS VARCHAR)' INTO primary_key_value USING NEW;
+                    END IF;
+                  END IF;
+                  IF TG_OP = 'DELETE' OR TG_OP = 'UPDATE' THEN
+                    EXECUTE 'SELECT CAST($1 . '|| column_name ||' AS TEXT)' INTO old_value USING OLD;
+                    IF primary_key_value IS NULL AND primary_key_column IS NOT NULL THEN
+                      EXECUTE 'SELECT CAST($1 . '|| primary_key_column ||' AS VARCHAR)' INTO primary_key_value USING OLD;
+                    END IF;
+                  END IF;
+
                   IF TG_OP != 'UPDATE' OR new_value != old_value OR (TG_OP = 'UPDATE' AND ( (new_value IS NULL AND old_value IS NOT NULL) OR (new_value IS NOT NULL AND old_value IS NULL))) THEN
                     INSERT INTO audit_log("operation",
                                           "table_name",
